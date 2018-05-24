05f30206e5ba0517e194484c4ef7d795452faaf8
gregsmith446 <gregsmith446@gmail.com>
2018-05-24 00:13:27 +0000

    completed HTML entities

---
+++ intermediate/HTML-entities.js
@@ -0,0 +1,33 @@
+//&, <, >, " (double quote), and ' (apostrophe)
+
+var hashMap = {'&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&apos;'};
+
+function convertHTML(str) {
+    var strArray = str.split('');
+    strArray = strArray.map(function(char) {
+        if (hashMap[char]){
+            return hashMap[char];
+        }
+        return char;
+    });
+    str = strArray.join('');
+    return str;
+}
+
+console.log(convertHTML("Dolce & Gabbana"));
+convertHTML('Stuff in "quotation marks"');
+
+// REFACTORED HTML AGENTS
+
+//&, <, >, " (double quote), and ' (apostrophe)
+
+// var hashMap = {'&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot', "'": '&apos'};
+
+// function convertHTML(str) {
+//     var strArray = str.split('');
+//     strArray = strArray.map(function(char) {return hashMap[char] ? hashMap[char] : char;});
+//     str = strArray.join('');
+//     return str;
+// }
+
+// console.log(convertHTML("Dolce & Gabbana"));
