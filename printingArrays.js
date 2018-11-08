243e9028a2c3687f03a93b432f579d1cbea54e3b
gregsmith446 <gregsmith446@gmail.com>
2018-11-08 16:35:17 +0000

    refreshing on arrays

---
+++ printingArrays.js
@@ -0,0 +1,18 @@
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function printGolf(par, strokes) {
+    
+    for (var i = 0; i < names.length; i++) {
+        // console.log(names[i]);
+        // console.log(names);
+        return console.log(names.toString());
+
+    }
+}
+
+printGolf();
+
+// can print the names array in multiple ways
+// print the whole array with console.log()
+// print the contents of the array via a for loop + console.log()
+// print the contents of the array as a string, use 'names.toString()'
