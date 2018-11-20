3986b7af5eb7395460942be611ff4735561ca296
gregsmith446 <gregsmith446@gmail.com>
2018-11-20 16:08:26 +0000

    destructure arrays

---
+++ ES6/destructureArrays.js
@@ -0,0 +1,11 @@
+// Code uses array destructuring assignment to swap values of a and b
+
+let a = 8, b = 6;
+(() => {
+  "use strict";
+  // change code below this line
+  [a, b] = [b, a];
+  // change code above this line
+})();
+console.log(a); // should be 6
+console.log(b); // should be 8
