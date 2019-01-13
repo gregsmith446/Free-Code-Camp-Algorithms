386f30a4991cb4288d6f18c76688f7093cd13d5c
gregsmith446 <gregsmith446@gmail.com>
2019-01-13 17:25:58 +0000

    making objects

---
+++ BasicDataStructures/13objectsKeyValue.js
@@ -0,0 +1,15 @@
+// At their most basic, objects are just collections of key-value pairs
+// Add three more entries: bananas with a value of 13, grapes with a value of 35, and strawberries with a value of 27.
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+
+// access using bracket notation
+foods['bananas'] = 13;
+foods['grapes'] = 35;
+foods['strawberries'] = 27;
+
+console.log(foods);
